57e5667274019d80fcae53b9ebdaf3a01e37bd08
Felix Guerrero <felixcriv@gmail.com>
2015-02-22 16:15:23 -0500

    fix

--- server.js
+++ server.js
@@ -10,21 +10,13 @@ app.set('port', (process.env.PORT || 5000));
 // Add headers
 app.use(function(req, res, next) {
 
-    // Website you wish to allow to connect
+    //Connect
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
+    // Allow
     res.setHeader('Access-Control-Allow-Methods', 'GET');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
     res.setHeader('Vary', 'Accept-Encoding'); 
-    
 
     // Pass to next layer of middleware
     next();
