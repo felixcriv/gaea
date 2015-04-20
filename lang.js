44898a9b561011137faff0bee92648078cc5f804
Felix Guerrero <felixcriv@gmail.com>
2015-04-20 19:31:01 -0400

    added Italian

--- lang.js
+++ lang.js
@@ -2,18 +2,39 @@
 
 var i18n = {
 
-    "es": {
-        "norte": "N",
-        "sur": "S",
-        "este": "E",
-        "oeste": "W",
-        "sureste": "SE",
-        "noreste": "NE",
-        "suroeste": "SW",
-        "noroeste": "NW"
+    "en-US": {
+
+        "coordinates": {
+            "norte": "North",
+            "sur": "South",
+            "este": "East",
+            "oeste": "West",
+            "sureste": "South East",
+            "noreste": "North East",
+            "suroeste": "South West",
+            "noroeste": "North West"
+        },
+
+        "de": "of"
+    },
+
+    "it":{
+        "coordinates": {
+            "norte": "Nord",
+            "sur": "Sud",
+            "este": "East",
+            "oeste": "Ovest",
+            "sureste": "Sud-Est",
+            "noreste": "Nord Est",
+            "suroeste": "Sud Ovest",
+            "noroeste": "Nord Ovest"
+        },
+
+        "de": "di"
     }
+
 }
 
-exports.translate = function(lang, str){
-	return i18n[lang][str];
+exports.translate = function(lang, str) {
+    return i18n[lang]['coordinates'][str] + " " + i18n[lang]['de'];
 }
