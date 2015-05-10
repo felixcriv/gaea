b493834c57863d87806e7243c5e1c571ad0631b8
Felix Guerrero <felixcriv@gmail.com>
2015-05-10 17:31:43 -0400

    config now is a file

---
+++ config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+(function() {
+
+    var Config = (function() {
+
+        var Config = function() {};
+
+
+        Config.prototype.options = (function options() {
+
+            return {
+                server: "http://www.funvisis.gob.ve",
+                recent_events: '/sis_reciente.php',
+                post_data: {
+                    "xjxfun": "actualizar",
+                    "xjxr": new Date().getTime()
+                }
+            }
+        })();
+
+        return Config;
+    })();
+
+    if(typeof module != 'undefined' && module.exports != 'undefined'){
+    	module.exports = new Config();
+    }else{
+    	window.Config = new Config();
+    }
+})();
